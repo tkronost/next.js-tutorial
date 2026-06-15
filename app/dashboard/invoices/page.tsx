34d5514849b36e0dbbb78e53c88de9d471b2b5c7
tkronost <tkronost03@gmail.com>
2026-06-15 17:09:24 -0300

    Agrego titulo (metadatos) a Login, Create Invoice, edit invoice, Dashboard

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -8,7 +8,6 @@ import { Suspense } from 'react';
 import { fetchInvoicesPages } from '@/app/lib/data';
 import { Metadata } from 'next';
 
-
 export const metadata: Metadata = {
   title: 'Invoices',
 };
